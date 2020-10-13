75b046071d7dca45153298d0725d03f338d66a95
FahimKawsar454884897 <53717504+FahimKawsar454884897@users.noreply.github.com>
2020-10-13 16:54:40 +0930

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,25 +4,37 @@ var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
 var urlsToCache = [
   '/',
   '/js/app.js',
-  '/recipe/makerecipe.html'
-  '/recipe/style1.css'
-  '/shopping/shopping.html'
-  '/toprecipe/topRecipe.html'
-  '/video/makevideo1.html'
-  '/home.html'
-  '/index.html'
-  '/manifest.json'
-  '/styles.css'
+  '/recipe/makerecipe.html',
+  '/recipe/style1.css',
+  '/shopping/shopping.html',
+  '/toprecipe/topRecipe.html',
+  '/video/makevideo1.html',
+  '/home.html',
+  '/index.html',
+  '/manifest.json',
+  '/styles.css',
   '/sw.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+      caches.open(staticCacheName).then( cache => {
+        console.log('caching cell assets');
+          cache.addAll(assets)
       })
-  );
-});
+    )
+  
+  })
+  
+  //activate event
+  self.addEventListener('activate', evt => {
+    console.log('service worker activated');
+  })
+  
+  //fetch event
+  self.addEventListener('fetch', evt => {
+    console.log('fetch Event', evt);
+  })
+    
+  
+
