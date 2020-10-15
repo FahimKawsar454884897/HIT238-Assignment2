17745f9723076c73dab6646b306be4220f3fc3b1
FahimKawsar454884897 <53717504+FahimKawsar454884897@users.noreply.github.com>
2020-10-15 20:12:21 +0930

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/FahimKawsar454884897/HIT238-Assignment2/sw.js')
+       .then((reg) => console.log('service worker registered', reg))
+       .catch((err) => console.log('sercice worker not registered', err));
+}
